dd99b9aadfe17cec0ffd76a740d8b55d9ecdf235
glytchy <colmmcdonnell@gmail.com>
2017-11-02 21:10:29 +0000

    Added Apache 2.0 license header to all Java classes

--- src/main/java/org/glitch/dragoman/store/mongo/EmbeddedMongoVerticle.java
+++ src/main/java/org/glitch/dragoman/store/mongo/EmbeddedMongoVerticle.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.glitch.dragoman.store.mongo;
 
 import de.flapdoodle.embed.mongo.Command;
@@ -25,14 +41,6 @@ import javax.inject.Inject;
 public class EmbeddedMongoVerticle extends AbstractVerticle {
     private static final Logger logger = LoggerFactory.getLogger(MongoModule.class);
     private static final Logger mongoLogger = LoggerFactory.getLogger("embedded-mongo");
-
-    // https://github.com/flapdoodle-oss/de.flapdoodle.embed.mongo#usage---optimization
-    //    private static final MongodStarter starter = MongodStarter.getInstance(
-    //            new RuntimeConfigBuilder()
-    //                    .defaults(Command.MongoD)
-    //                    .processOutput(ProcessOutput.getInstance("embedded-mongo", mongoLogger))
-    //                    .build()
-    //    );
 
     private final int port;
     private final CannedDatasetsWriter cannedDatasetsWriter;
